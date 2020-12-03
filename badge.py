20b73bd5481243c020870e1ffba1092040e6d5d1
alebas-fr <58038165+alebas-fr@users.noreply.github.com>
2020-12-03 11:42:07 +0100

    Add files via upload

--- badge.py
+++ badge.py
@@ -12,9 +12,9 @@ import sys
 from PIL import Image,ImageTk
 
 
-StateOfCardReader = 0 # 0 Wait a card, 1 Card is valid, 2 card is not valide, 3 reader is currenty not available, 4 sent old log
+StateOfCardReader = 0 # 0 Wait a card, 1 Card is valid, 2 card is not valide, 4 sent old log
 AuthError = ""
-
+WindowIsOpen = True
 
 FileToWrite = None
 textShell=""
@@ -47,57 +47,56 @@ ImageCanva.pack()
 ErrorLabel = Tk.Label(Window,text=AuthError,bg="#181818",fg="white")
 ErrorLabel.pack()
 
-StateOfCardReader=0
-AuthError = ""
-textShell = os.popen(cmd).read().strip()
-print("Texte Shell : ",textShell)
-#Read the card
-if (textShell.startswith("UID")):
-	UIDWithSpace = textShell.split(":")[1]
-	UIDWithoutSpace = UIDWithSpace.replace("  ",":").strip()
-	print(UIDWithoutSpace)
-	try:
-		auth = Authentication(rfid=UIDWithoutSpace)
-		auth.add_passage_to_log()
-	except KeyError:
-		StateOfCardReader = 2
-	except ConnectionError :
-		AuthError = "Attention le serveur est innacesible sauvegarde des passages en local"
-		StateOfCardReader = 1
-		SendOldLog = True
-		MomentOfNonSent = time.time()
-		date = datetime.now()
-		FileToWrite = open("HistoryOfPassage.log",mode="a")
-		FileToWrite.writelines(UIDWithoutSpace+","+str(date)+"\n")
-		FileToWrite.close()
-	except :
-		print("ERROR INCONUE")
-		AuthError = "Erreur INCONUE"
-		StateOfCardReader = 2
-elif ("ERROR" in textShell):
-	print("On ne trouve pas le lecteur de carte NFC")
-	AuthError = "lecteur de carte non branché"
-	StateOfCardReader = 3
-print(AuthError,StateOfCardReader)
-time.sleep(2)
-# Send the oldlog if the Dokos Server had problem
-if SendOldLog and (time.time()-MomentOfNonSent<60):
-	FileToRead = open("HistoryOfPassage.log",mode="r")
-	OldLogString = FileToRead.read()
-	FileToRead.close()
-	os.remove("HistoryOfPassage.log")
-	OldLogList = OldLogString.split("\n")
-	StateOfCardReader = 4
-	AuthError = "Envoie des données locales au serveur"
-	for e in OldLogList:
+while WindowIsOpen
+	StateOfCardReader=0
+	AuthError = ""
+	textShell = os.popen(cmd).read().strip()
+	print("Texte Shell : ",textShell)
+	#Read the card
+	if (textShell.startswith("UID")):
+		UIDWithSpace = textShell.split(":")[1]
+		UIDWithoutSpace = UIDWithSpace.replace("  ",":").strip()
+		print(UIDWithoutSpace)
 		try:
-			auth = Authentication(rfid=e.split(",")[0])
-			auth.add_passage_to_log(date=e.split(",")[1])
-		except ConnectionError:
+			auth = Authentication(rfid=UIDWithoutSpace)
+			auth.add_passage_to_log()
+		except KeyError:
+			StateOfCardReader = 2
+		except ConnectionError :
+			AuthError = "Attention le serveur est innacesible sauvegarde des passages en local"
+			StateOfCardReader = 1
 			SendOldLog = True
 			MomentOfNonSent = time.time()
+			date = datetime.now()
 			FileToWrite = open("HistoryOfPassage.log",mode="a")
-			FileToWrite.writelines(e.split(",")[0]+","+e.split(",")[1]+"\n")
+			FileToWrite.writelines(UIDWithoutSpace+","+str(date)+"\n")
 			FileToWrite.close()
+		except :
+			print("ERROR INCONUE")
+			AuthError = "Erreur INCONUE"
+			StateOfCardReader = 2
+	print(AuthError,StateOfCardReader)
+	time.sleep(2)
+	# Send the oldlog if the Dokos Server had problem
+	if SendOldLog and (time.time()-MomentOfNonSent<60):
+		FileToRead = open("HistoryOfPassage.log",mode="r")
+		OldLogString = FileToRead.read()
+		FileToRead.close()
+		os.remove("HistoryOfPassage.log")
+		OldLogList = OldLogString.split("\n")
+		StateOfCardReader = 4
+		AuthError = "Envoie des données locales au serveur"
+		for e in OldLogList:
+			try:
+				auth = Authentication(rfid=e.split(",")[0])
+				auth.add_passage_to_log(date=e.split(",")[1])
+			except ConnectionError:
+				SendOldLog = True
+				MomentOfNonSent = time.time()
+				FileToWrite = open("HistoryOfPassage.log",mode="a")
+				FileToWrite.writelines(e.split(",")[0]+","+e.split(",")[1]+"\n")
+				FileToWrite.close()
 
 Window.mainloop()
+print(WindowIsOpen)
+WindowIsOpen = False
