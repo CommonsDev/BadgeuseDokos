e56415b5a17d2550d59b59e7d89f1f6356e28a28
alebas-fr <58038165+alebas-fr@users.noreply.github.com>
2020-12-16 19:13:49 +0100

    Add files via upload

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 # Dokos Client Key
-dokos_client = ""
+dokos_client = "c1ec7c585175617"
 # Dokos Token Secret
-dokos_token = ""
+dokos_token = "770219117ee97ea"
 
 # The URL of the API
-api_url = ""
+api_url = "http://192.168.1.26:8000/api/"
 
 # The doctype name defined in Dokos
-badge_allocation_resource_name = ""
-passage_log_resource_name = ""
+badge_allocation_resource_name = "Allocation Of Badge"
+passage_log_resource_name = "History Of Passage"
 
 #Could be None, In or Out
-badging_type = ""
+badging_type = "None"
