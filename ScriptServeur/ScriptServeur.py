48bebd161bec21d6674fcad8505678d61fce0fb8
alebas-fr <58038165+alebas-fr@users.noreply.github.com>
2021-01-18 19:38:43 +0100

    Add files via upload

--- ScriptServeur/ScriptServeur.py
+++ ScriptServeur/ScriptServeur.py
@@ -9,10 +9,13 @@ def insert_document():
 	Insert a document in the doctype History Of Presence and paired the document
 	"""
 	#Get the unit and item from the Doctype Badge Management Settings
+
 	badge = frappe.db.get_list('Badge Management Settings',fields= ['name'])
 	unit = frappe.db.get_value('Badge Management Settings',badge[0].name,'default_unit')
 	item = frappe.db.get_value('Badge Management Settings',badge[0].name,'default_item')
+
 	# Get all the document in the doctype History Of Passage
+
 	allUnpairPassage = frappe.db.get_list('History Of Passage',
 	filters={
 	'is_paired' : 0,
@@ -20,10 +23,14 @@ def insert_document():
 	},
 	fields= ['name','date','customer','user'],
 	)
+
 	# From the last passage get the user and the customer
+
 	user = frappe.db.get_value("History Of Passage",allUnpairPassage[0].name,'user')
 	customer = frappe.db.get_value("History Of Passage",allUnpairPassage[0].name,'customer')
+
 	#Search a passage of the same user and customer in the same day which is not paired
+
 	unpairedDoc = frappe.db.get_list('History Of Passage',
 	filters={
 	'customer' : customer,
@@ -31,9 +38,13 @@ def insert_document():
 	'date' : ['>',frappe.utils.nowdate()],
 	'user' : user},
 	fields= ['name', 'date'])
+
 	# Verify if the user and customer have 2 passage unpair 
+
 	if len(unpairedDoc)==2:
+		
 		# If they have 2 passage unpair create a Presence in History Of Presence
+
 		doc = frappe.get_doc({
 			'doctype' : 'History Of Presence',
 			'user' : frappe.db.get_value("History Of Passage",unpairedDoc[0].name,'user'),
@@ -46,7 +57,9 @@ def insert_document():
 			'unit' : unit
 		})
 		doc.insert(ignore_permissions=True)
+
 		# After the creation of the passage, set the value is_paired of the two passage to True
+		
 		frappe.db.set_value('History Of Passage', unpairedDoc[0].name,'is_paired', 1)
 		frappe.db.set_value('History Of Passage', unpairedDoc[1].name,'is_paired', 1)
 	
